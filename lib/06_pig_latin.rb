b6dfdbe28844b497f3ceea380bee4fe89d738f10
Manonn <manon.nezot@gmail.com>
2022-10-11 02:13:21 +0200

    exo_06_fin

--- lib/06_pig_latin.rb
+++ lib/06_pig_latin.rb
@@ -1,29 +1,21 @@
-=begin
-def explications
-    si commence a, e, i, o, u -> ajouter "ay" à la fin 
-    sinon déplacer la premiere lettre à la derniere place et rajouter "ay" à la fin
+#Bonne chance Manon
 
-    si commence par 2 consonnes, déplacer les 2 consonnes à la fin et rajouter "ay" après
-
-    deux mots -->  ça doit fonctionner
-
-    trois consonnes --> on les met à la fin + ay
-
-    qu -> 1 compte quand même comme deux consonnes 
+def translate(text)
+    text.split.map { |w| translate_single_word(w) }.join(' ')
 end
 
-3 premières lettres
-    -> son type (consonne ou voyelle) + exeption "qu"
-    -> puis rajouter "ay" à la fin
-=end
-
-def translate(word)
-  word.split.each do |i|
-    if i[0] == "a" || i[0] =="e" || i[0] =="i" || i[0] =="o" || i[0] =="u"
-      return word + "ay"
-    else
-      return word[1..-1] + word[0] + "ay"
-    end   
-  end
+def translate_single_word(word)
+  count = consonant_count(word)
+  word[count..] + word[0, count] + "ay"
 end
 
+def consonant_count(word)
+    return 2 + consonant_count(word[2..]) if word.start_with?('qu')
+    return 1 + consonant_count(word[1..]) if consonant?(word.chars.first)
+    return 0
+end
+
+# tells if a letter is a consonant or
+def consonant?(letter)
+  !['a', 'e', 'i', 'o', 'u'].include?(letter)
+end
